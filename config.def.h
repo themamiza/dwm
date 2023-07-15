318ab60f53451d277d507f7eebb9b126bb643987
mamiza <drogoniza@gmail.com>
2023-07-16 02:42:37 +0330

    Changed to a proper font

--- config.def.h
+++ config.def.h
@@ -17,8 +17,8 @@ static                int smartgaps = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 32;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Iosevka:size=12", "NotoColorEmoji:size=10" };
+static const char dmenufont[]       = "Iosevka:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
